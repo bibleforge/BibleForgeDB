cf1dcee38785d9e06e1e481708cf74f7faabff29
BibleForge <info@bibleforge.com>
2010-08-18 03:10:30 -0500

    Improved user-friendliness and added database creation support as well as support for database names with special characters.

--- import.sh
+++ import.sh
@@ -11,10 +11,45 @@
 # This program imports the databases used by BibleForge into a MySQL database.
 # This is designed for Unix-like systems.
 
-echo "Enter database:";
+printf "Enter database name (bf): "
 read mysqlDataBase
 
-echo "Enter username:";
+if [ "$mysqlDataBase" = "" ]; then
+    mysqlDataBase="bf"
+fi
+
+
+printf "Enter username (root): "
 read mysqlUserName
 
-gzip -d -c BibleForge.sql.gz | mysql -u $mysqlUserName -p $mysqlDataBase
+if [ "$mysqlUserName" = "" ]; then
+    mysqlUserName="root"
+fi
+
+
+printf "Enter password: "
+# Turn off typing echo.
+stty -echo
+read mysqlPassword
+# Turn typing echo back on.
+stty echo
+# Since "echo" is turned off, the return character is not caught, so the script needs to insert a line break manually.
+# NOTE: This creates two line breaks because echo automatically adds a line break.
+echo -e "\n"
+
+# Check to see if the database exists.
+# NOTE: Backticks (`) execute the command and store the results.
+# NOTE: 2>&1 gets STDERR and redirects it to STDOUT.
+# NOTE: The -e argument makes mysql quite immediately.
+RES=`mysql -u $mysqlUserName -p$mysqlPassword "$mysqlDataBase" -e "show tables;" 2>&1`
+
+# See if an error message was returned.  Error 1049 means no database was found.
+# NOTE: Get the trailing space to make sure that we get the entire error number.
+if [ "${RES:0:11}" = "ERROR 1049 " ]; then
+    echo "Attempting to create database '$mysqlDataBase'"
+    mysql -u $mysqlUserName -p$mysqlPassword -e "CREATE DATABASE \`$mysqlDataBase\`;"
+fi
+
+echo "Importing databases...please wait...";
+
+gzip -d -c BibleForge.sql.gz | mysql -u $mysqlUserName -p$mysqlPassword "$mysqlDataBase"

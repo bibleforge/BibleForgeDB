30cbccb01e7e002b669e4d944fcf8ed7483fb0b6
BibleForge <info@bibleforge.com>
2012-05-25 01:38:26 +0800

    Added the English Sphinx grammatical pseudo-table.

--- sphinx.sql
+++ sphinx.sql
@@ -40,3 +40,16 @@ CREATE TABLE `verse_text_en_em` (
     `query` varchar(3072) NOT NULL,
     KEY `query` (`query`)
 ) ENGINE=SPHINX DEFAULT CHARSET=latin1 CONNECTION='sphinx://127.0.0.1:9312/verse_text_en_em';
+
+
+
+-- First, drop it if it exists.
+DROP TABLE IF EXISTS `morphological_en`;
+
+CREATE TABLE `morphological_en` (
+    `id` int(10) unsigned NOT NULL,
+    `weight` int(11) NOT NULL,
+    `query` varchar(3072) NOT NULL,
+    `verseID` int(11) NOT NULL,
+    KEY `query` (`query`)
+) ENGINE=SPHINX DEFAULT CHARSET=latin1 CONNECTION='sphinx://127.0.0.1:9312/morphological_en';
